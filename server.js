53c1f7b880a827a3f8160d6d5879a7e92c42d932
Tyler Watson <tylerww91@gmail.com>
2023-05-28 11:33:47 -0700

    adds stripe, adds store items

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: 'Bubbbler' }],
+  [2, { priceInCents: 20000, name: 'Klein' }],
+]);
+
 const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
 
