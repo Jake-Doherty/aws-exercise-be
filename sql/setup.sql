67b238d4901a5952c215915f2197827e82651894
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-06-08 15:47:18 -0700

    creates cognito and stripe user tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,10 +2,14 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 
-CREATE TABLE users (
+CREATE TABLE cognito_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR,
-  password_hash VARCHAR NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL
+  sub VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE stripe_customers (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  customer_id VARCHAR(255) NOT NULL,
+  user_id BIGINT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES cognito_users(id)
 );
